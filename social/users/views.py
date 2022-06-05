789621a72686768f3eab33f9ee48bc1505946712
haimy254 <haimanaare@gmail.com>
2022-06-05 19:07:59 +0300

    created registration

--- social/users/views.py
+++ social/users/views.py
@@ -1,17 +1,27 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth import authenticate, login,logout
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
 # Create your views here.
-def login(request):
+def register(request):
+    form = UserCreationForm()
+    
     if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
-        return redirect ('base.html')
-    else:
-        messages.success('request',("The user is not registered"))
-        return redirect ('authentication/registration.html')
-    # return render(request,'authenticate/login.html')
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'form': form}
+    return render(request,'authenticate/registration.html', context)
+        
+def login(request):
+#     if request.method == "POST":
+#         username = request.POST.get("username")
+#         password = request.POST.get('password')
+#         user = authenticate(request, username=username, password=password)
+#     if user is not None:
+#         login(request, user)
+#         return redirect ('base.html')
+#     else:
+#         # messages.success('request',("The user is not registered"))
+#         return redirect ('authentication/registration.html')
+    return render(request,'authenticate/login.html')
