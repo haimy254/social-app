5a9f0b4b6fa7e0170b3b433741ce974e7bae4c14
haimy254 <haimanaare@gmail.com>
2022-06-05 17:22:58 +0300

    created a registration html and added login view

--- social/users/views.py
+++ social/users/views.py
@@ -4,4 +4,14 @@ from django.contrib import messages
 
 # Create your views here.
 def login(request):
-    return render(request,'authenticate/login.html')
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return redirect ('base.html')
+    else:
+        messages.success('request',("The user is not registered"))
+        return redirect ('authentication/registration.html')
+    # return render(request,'authenticate/login.html')
