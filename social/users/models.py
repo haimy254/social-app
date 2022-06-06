30371041308b68dafb51729901ec2f9aaebc263a
haimy254 <haimanaare@gmail.com>
2022-06-06 09:32:53 +0300

    created profile model

--- social/users/models.py
+++ social/users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    profile_photo= models.ImageField(upload_to='profile/')
+    bio = models.CharField(max_length=800)
+    
+   
