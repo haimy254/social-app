a475cfae6e2d38368b73d9b55ba53ff98cc465fd
haimy254 <haimanaare@gmail.com>
2022-06-06 11:37:00 +0300

    added profile and comment model

--- social/users/models.py
+++ social/users/models.py
@@ -5,4 +5,11 @@ class Profile(models.Model):
     profile_photo= models.ImageField(upload_to='profile/')
     bio = models.CharField(max_length=800)
     
-   
+class Comments(models.Model):
+    comment = models.CharField(max_length=800)
+    
+    def __str__(self):
+        return self.comment
+    
+    def save_comment(self):
+        self.save
