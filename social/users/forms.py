fd609abd6e68b65508d9cd9e3d6543b9951c764b
haimy254 <haimanaare@gmail.com>
2022-06-07 08:33:32 +0300

    added a form for images

--- social/users/forms.py
+++ social/users/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 
-from .models import Profile
+from .models import Image, Profile
 
 class CreateUserForm(UserCreationForm):
     class Meta:
@@ -15,3 +15,9 @@ class ProfileForm(forms.ModelForm):
         model = Profile
         fields = ['profile_photo','bio']
         templates = ()
+        
+class ImageForm(forms.ModelForm):
+    
+    class Meta:
+        model = Image
+        fields = ['images','image_name','image_caption',]
--- social/users/models.py
+++ social/users/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
@@ -6,7 +7,7 @@ class Profile(models.Model):
     bio = models.CharField(max_length=800)
     
 class Comments(models.Model):
-    comment = models.CharField(max_length=800)
+    comment = models.CharField(max_length=800, default='')
     
     def __str__(self):
         return self.comment
@@ -14,9 +15,14 @@ class Comments(models.Model):
     def save_comment(self):
         self.save
         
+class Likes(models.Model):
+    # like = model
+        pass
 class Image(models.Model):
-    image = models.ImageField(upload_to = 'image')
+    images = models.ImageField(upload_to = 'image/',default='')
     image_name= models.CharField(max_length=100)
     image_caption = models.CharField(max_length=500)
     profile = models.ForeignKey(Profile,on_delete= models.CASCADE)
+    comment = models.ForeignKey(Comments,on_delete= models.CASCADE )
+    likes = models.ManyToManyField(User, related_name='like_image')
     
